78f7c2d8c98d3b5e816238d9affcefe1531c2181
Juana-leyton <leytonjuanita3@gmail.com>
2020-10-29 17:09:28 -0500

    make it better

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const conectDB = () => {
-    mongoose.connect('mongodb+srv://Juana_leyton:Juana21042003@juanita.xbxya.mongodb.net/keyCodeBook?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }, (error) =>{
+    mongoose.connect('', { useNewUrlParser: true, useUnifiedTopology: true }, (error) =>{
         if (error){
             console.log('Error: ', error)
         }else{
